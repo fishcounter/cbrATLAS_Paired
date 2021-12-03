75daf7cd98a1454872d9768d0bc42d67668152ed
Richard Townsend <85508520+fishcounter@users.noreply.github.com>
2021-12-03 13:28:48 -0800

    updated main fn to include failCompare

--- R/AdjSurv.fn.R
+++ R/AdjSurv.fn.R
@@ -1,37 +1,40 @@
 
 #' @title Active Tag-Life-Adjusted Survival Modeling Fitting
 #'
+#' @description Analysis of release-recapture data from a study that uses active-tag technology.
+#' Estimates survival and detection probabilities, and if provided, the average probability
+#' a tag is active at each detection site.  These estimates are then used to adjust estimated survival for
+#' potential tag-failure that may be interpreted as mortality. Methods are documented in Program ATLAS 1.4:
+#' Active Tag Life Adjusted Survival (http://www.cbr.washington.edu/sites/default/files/manuals/ATLAS_1_4_Manual_0.pdf)
+#'
 #' @param taglife.file Optional. Name of .csv file with taglife times in 1st column.  other columns ignored.  Header expected.
 #' @param taghist.file  Required. dataframe or a filename of .csv with detection histories. 2 formats currently supported.
-#' @param taghist.format Format of tag detection histories: "atlas" or"flat"
+#' @param taghist.format Format of tag detection histories: "atlas" or"flat".
 #' "atlas" = format based on Program ATLAS input files. 8 columns.  Each tag has 1 line per possible detection site
 #' 	"flat" = format has 1 line per tag. tag.code, activation date, release date, and
 #' 	          1 column per detection site filled with 1st detection times at that site (current format: "%m/%d/%Y %H:%M")
 #' @param taglife.model Failure time model obs (fc_obj)
-#' @param num.release if more than 1 release group 1st column will be added to flat format file that will denote group name
 #' @param num.release if more than 1 release group 1st column will be added to flat format file that will denote group name (currently not working)
 #' @param num.bootstrap number of resamples to estimate additional variance on survival estimates, default is 1000
 #' @param adjust.cjs Should CJS estimates be adjusted for estimated tag-life? Logical.
 #' @param rounding Number of decimal places on estimates
 #' @param plot.taglife plot the estimated tag-life curve. Logical.
-#'
+#' @importFrom failCompare fc_fit fc_rank fc_select
 #' @return Tables of estimates for unadjusted survival model and, if the taglife adjustment is used, taglife model estimates and adjusted survival estimates
 #'
 #' @export
 #'
 AdjSurv.fn=function (taglife.file=NULL, taghist.file,taghist.format="atlas", taglife.model=NULL, num.release=1,
-                     num.bootstrap = 1000, adjust.cjs = T, rounding = 4, plot.taglife = T) {
                      num.bootstrap = 250, adjust.cjs = T, rounding = 4, plot.taglife = T) {
   if(is.data.frame(taghist.file)){data.file=taghist.file}else{
-    data.file = data.frame(read.csv(taghist.file, header = F, colClasses = c("character")))}
+    data.file = data.frame(utils::read.csv(taghist.file, header = F, colClasses = c("character")))}
   if(!is.null(taglife.file)){
     if(is.data.frame(taglife.file)){tag.life=taglife.file}else{
-    tag.life = read.csv(taglife.file, header = T)}
+    tag.life = utils::read.csv(taglife.file, header = T)}
     tag.life = sort(tag.life[, 1])
   }
-  library(failCompare)
 
-  # reformat detetion file if in Program ATLAS format from ATLAS to flat
+    # reformat detetion file if in Program ATLAS format from ATLAS to flat
   if(taghist.format == "atlas"){data.file=atlas2flat.fn(data.file)}
 
   rel.groups=unique(data.file[,1])
@@ -55,17 +58,17 @@ AdjSurv.fn=function (taglife.file=NULL, taghist.file,taghist.format="atlas", tag
     if(!is.null(taglife.file)){
     ################################################### Estimate tag-life curve
       if(is.null(taglife.model)){
-        mod_ls=fc_fit(time=taglife.data$tag_life_days,model=c('weibull','weibull3','gompertz','gamma','lognormal','llogis','gengamma'))
-        mod_ls_ranked=fc_rank(mod_ls)
+        mod_ls=failCompare::fc_fit(time=taglife.file$tag_life_days,model=c('weibull','weibull3','gompertz','gamma','lognormal','llogis','gengamma'))
+        mod_ls_ranked=failCompare::fc_rank(mod_ls)
 
         # currently, select best-fitting model automatically
-        taglife.fit=fc_select(mod_ls_ranked,model = as.character(mod_ls_ranked$"GOF_tab"[1,1]))
+        taglife.fit=failCompare::fc_select(mod_ls_ranked,model = as.character(mod_ls_ranked$"GOF_tab"[1,1]))
       }else{taglife.fit=taglife.model}
 
       if(plot.taglife){plot(taglife.fit)}
 
       ########### Estimate mean travel time to each site, use to calculate mean failure at each site
-      tt.rel2site=mean.tt2site.fn(data.in,num.period,site.names)
+      tt.rel2site=mean_tt2site.fn(data.in,num.period,site.names)
 
       mean.tag.p=cjs.taglife.corr(activetime.matrix=tt.rel2site$activetime.matrix,site.names=site.names,
                                 num.period=num.period,taglife.fit=taglife.fit,num.boots=0)
