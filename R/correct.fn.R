0e05b2cb3a4de59c203806a83b2cc7b052165430
Richard Townsend <85508520+fishcounter@users.noreply.github.com>
2021-12-03 13:54:17 -0800

    moved to its own file

---
+++ R/correct.fn.R
@@ -0,0 +1,20 @@
+#' @title Restrain uncorrected probability estimates between 0 and 1
+#'
+#' @description The CJS likelihood estimates are currently bounded between 0 and
+#' 1, which may bias survival estimates low in cases of low detection probability.
+#' This is not usually the case with active tag technology with typically high
+#' detection rates.  The CJS estimate may estimated greater than 100%, and this may
+#' be included in later iterations.
+#'
+#' @param x value to be rounded up to be between 0 to 1
+#'
+#' @return rounded value
+#' @export
+#'
+correct.fn=function(x){
+  # keep probabilities between 0 and 1
+  x[x<0.0000001]=1e-10
+  x[x>0.9999999]=1-1e-10
+  return(x)
+}
+
