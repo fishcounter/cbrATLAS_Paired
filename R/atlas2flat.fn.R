27f036dd315e806707d05b670a452915efb71b37
Richard Townsend <85508520+fishcounter@users.noreply.github.com>
2021-12-03 13:39:24 -0800

    added "stats::" to reshape fn documentation to function

--- R/atlas2flat.fn.R
+++ R/atlas2flat.fn.R
@@ -1,13 +1,18 @@
 
+#' @title converts ATLAS data file to a flat file format
+#'
+#' @description The ATLAS file format (detailed in the ATLAS 1.4 manual, pg 17) is a vertical file
+#' with 1 line per detection site.  The input format for the cbrATLAS uses a flat file format, which details
+#' the history of eacg tag on 1 line, including release and dection times at each site.
 #'
 #' @param data.in table: columns = release group name, bin number, tag id, tag activation date/time, tag release date/time, site name, detection (1=yes, 0 = no), detection date/time
 #'
-#' @return table: columns = release group name, bin number, tag id, activation date/time, tag release date/time, 1 column per site name with detection time (blank if no detection)
+#' @return table with columns: release group name, bin number, tag id, activation date/time, tag release date/time, 1 column per site name with detection time (blank if no detection)
 #' @export
 #'
 atlas2flat.fn=function(data.in){
   names(data.in)[7:8]=c("detect","time")
-  data.out = reshape(data.in, v.names = c("detect","time"), idvar = c("V1","V2","V3","V4","V5"),
+  data.out = stats::reshape(data.in, v.names = c("detect","time"), idvar = c("V1","V2","V3","V4","V5"),
                      timevar = "V6", direction = "wide")
   data.out=data.out[,c(1:5,grep("detect",names(data.out)),grep("time",names(data.out)))]
   names(data.out)=sub("detect.","",names(data.out))
