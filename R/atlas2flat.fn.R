dde597ecaaab46630849f993e720a05f51676b64
Richard Townsend <85508520+fishcounter@users.noreply.github.com>
2025-08-07 13:48:45 -0700

    Update atlas2flat.fn.R
    adjustments to formatting of description for readability

--- R/atlas2flat.fn.R
+++ R/atlas2flat.fn.R
@@ -1,13 +1,17 @@
 
 #' @title Converts ATLAS data file to a flat file format
 #'
-#' @description cbrATLAS allows the mark-recapture data to be loaded in one of two file formats. The ATLAS file format (detailed in the \href{http://www.cbr.washington.edu/sites/default/files/manuals/ATLAS_1_4_Manual_0.pdf}{ATLAS 1.4 manual}, page 17) is a vertical file
-#' with one line per detection site. The default input format for the cbrATLAS package uses a flat file format, which details
-#' the history of each tag on one line, including release and detection times at each site.  This function will converts an ATLAS formatted file to a flat file.
+#' @description cbrATLAS allows the mark-recapture data to be loaded in one of two file formats. The ATLAS file format 
+#' 	(detailed in the \href{https://www.cbr.washington.edu/analysis/apps/atlas}{ATLAS 1.9 manual}, Appendix A) is a vertical file
+#' 	with one line per detection site. The default input format for the cbrATLAS package uses a flat file format, which details
+#' 	the history of each tag on one line, including release and detection times at each site.  
+#' 	This function will converts an ATLAS formatted file to a flat file.
 #'
-#' @param data.in Table: columns = release group name, bin number, tag id, tag activation date/time, tag release date/time, site name, detection (1 = yes, 0 = no), detection date/time
+#' @param data.in Table: columns = release group name, bin number, tag id, tag activation date/time, tag release date/time, site name, 
+#' 	detection (1 = yes, 0 = no), detection date/time
 #'
-#' @return Returns a table with columns: release group name, bin number, tag id, activation date/time, tag release date/time, one column per site name with detection time (blank if no detection)
+#' @return Returns a table with columns: release group name, bin number, tag id, activation date/time, tag release date/time, one column 
+#' 	per site name with detection time (blank if no detection)
 #' @export
 #'
 atlas2flat.fn=function(data.in){
