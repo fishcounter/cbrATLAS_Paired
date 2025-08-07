3a09d7395d05d54002a42c06606d8e432ac8a81d
Richard Townsend <85508520+fishcounter@users.noreply.github.com>
2025-08-07 15:57:34 -0700

    Update harmonic.R

--- R/harmonic.R
+++ R/harmonic.R
@@ -10,8 +10,8 @@
 #'
 harmonic=function(t.in){
    w=1/t.in
-  t.out=length(w)/sum(w)
-  t.var=(stats::var(w)/length(w))*(t.out^4)
-  t.se=sqrt(t.var)
-  return(paste(c(sprintf("%.4f",t.out),"  (",sprintf("%.4f",t.se),")      ",length(t.in)),collapse=""))
+   t.out=length(w)/sum(w)
+   t.var=(stats::var(w)/length(w))*(t.out^4)
+   t.se=sqrt(t.var)
+   return(paste(c(sprintf("%.4f",t.out),"  (",sprintf("%.4f",t.se),")      ",length(t.in)),collapse=""))
 }
